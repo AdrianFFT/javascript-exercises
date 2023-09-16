7b106580a2edc1689dd1db53d56a09f3d1036c40
Adrian Farcas <farcasadriantiberiu@gmail.com>
2023-09-16 14:25:47 +0300

    Solutions for assignments of Javascript Fundamentals 4 Chapter

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, nr) {
+    if (nr<0){
+        return 'ERROR';
+    }
+    result='';
+    for (let i=0; i<nr; i++){
+        result+=str;
+    }
+    return result;
 };
 
 // Do not edit below this line
